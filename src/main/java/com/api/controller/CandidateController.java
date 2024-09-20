8fd4dcd48aa2faac2942fbeeff79952e878c713b
MCT <zoltancao@gmail.com>
2024-09-19 22:24:08 -0300

    feature/LOC-11/Atualizada criada a controller de Candidatos

--- src/main/java/com/api/controller/CandidateController.java
+++ src/main/java/com/api/controller/CandidateController.java
@@ -1,9 +1,15 @@
 package com.api.controller;
 
+import com.api.domain.entity.Candidate;
+import com.api.domain.entity.User;
 import com.api.domain.service.CandidateService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/candidates")
@@ -11,4 +17,13 @@ public class CandidateController {
 
     @Autowired
     private CandidateService candidateService;
+
+
+    @GetMapping
+    public ResponseEntity<List<Candidate>> getAllCandidates() {
+        List<Candidate> candidates = candidateService.getAllCandidates();
+        return ResponseEntity.ok(candidates);
+    }
+
+
 }
