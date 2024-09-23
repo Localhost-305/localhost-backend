d5cbb47ccb39a065c6f021d328f39c9a51062363
MCT <zoltancao@gmail.com>
2024-09-23 20:38:14 -0300

    feature/LOC-11/Ajustado o repository

--- src/main/java/com/api/domain/repository/FactApplicationRepository.java
+++ src/main/java/com/api/domain/repository/FactApplicationRepository.java
@@ -2,8 +2,7 @@ package com.api.domain.repository;
 
 import com.api.domain.entity.FactApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface FactApplicationRepository extends JpaRepository<FactApplication, Long> {
-   @Query
+
 }
