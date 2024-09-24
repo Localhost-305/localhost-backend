ce0181ece80bd91d0c7ad329e773d6c3f95f4fb7
MCT <zoltancao@gmail.com>
2024-09-23 22:47:07 -0300

    feature/LOC-11/Criada a pasta Util

--- src/main/java/com/api/domain/service/FactApplicationService.java
+++ src/main/java/com/api/domain/service/FactApplicationService.java
@@ -1,15 +1,12 @@
 package com.api.domain.service;
 
-import com.api.domain.MapObjectList;
-import com.api.domain.entity.FactApplication;
+import com.api.domain.util.MapObjectList;
 import com.api.domain.repository.FactApplicationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Map;
 
 @Service
 public class FactApplicationService {
--- src/main/java/com/api/domain/util/MapObjectList.java
+++ src/main/java/com/api/domain/util/MapObjectList.java
@@ -1,4 +1,4 @@
-package com.api.domain;
+package com.api.domain.util;
 
 
 
