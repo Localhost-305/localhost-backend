08f64b69a3caf84df570297d683ab780dda8712e
MCT <zoltancao@gmail.com>
2024-10-08 22:11:37 -0300

    feature/LOC-31 - Ajustes no código

--- src/main/java/com/api/domain/repository/FactApplicationRepository.java
+++ src/main/java/com/api/domain/repository/FactApplicationRepository.java
@@ -12,9 +12,6 @@ public interface FactApplicationRepository extends JpaRepository<FactApplication
  List<Object[]> getAllFactorUserByDate(@Param("startDate") LocalDate startDate, @Param("endDate")LocalDate endDate);
 
 
-// @Query(value = "select j.job_title,COUNT(a.candidate_id) AS numero_de_candidatos from fact_applications f left join dim_jobs j on j.job_id = f.job_id Where :startDate<=j.opening_date and :endDate>=j.closing_date  group By j.job_title" , nativeQuery = true)
-// List<Object[]> getAllFactorCandidateByDate(@Param("startDate") LocalDate startDate, @Param("endDate")LocalDate endDate);
-
  @Query(value = "SELECT j.job_title, COUNT(a.candidate_id) AS numero_de_candidatos " +
          "FROM fact_applications a " +
          "INNER JOIN dim_jobs j ON a.job_id = j.job_id " +
