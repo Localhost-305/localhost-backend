c17cacf43dd72ecb287831023535d4794eb020f6
MCT <zoltancao@gmail.com>
2024-10-21 22:21:17 -0300

    feature/LOC-64 Criadas as classes de controller, service e repositoy para Quantidade de candidaturas

---
+++ src/main/java/com/api/domain/repository/QuantityApplicationsRepository.java
@@ -0,0 +1,7 @@
+package com.api.domain.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class QuantityApplicationsRepository {
+}
---
+++ src/main/java/com/api/domain/service/QuantityApplicationsService.java
@@ -0,0 +1,8 @@
+package com.api.domain.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class QuantityApplicationsService {
+}
