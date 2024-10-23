4a75b05c6381df8dbab1dbfaad43dc47e74227f9
MCT <zoltancao@gmail.com>
2024-10-22 22:37:04 -0300

    feature/LOC-64 Realizando teste para query

--- src/main/java/com/api/domain/repository/QuantityApplicationsRepository.java
+++ src/main/java/com/api/domain/repository/QuantityApplicationsRepository.java
@@ -3,10 +3,17 @@ package com.api.domain.repository;
 
 import com.api.domain.entity.FactHiring;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 
 @Repository
 public interface QuantityApplicationsRepository extends JpaRepository<FactHiring, Long> {
+
+    @Query(value = "SELECT(count)(candidate_id) as allCandidate  FROM FactHirings",nativeQuery = true)
+    Long countAllCandidates();
+
+
 }
 
 
