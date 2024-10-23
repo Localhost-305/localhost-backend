1daf5cf59740f7c5c2753d97a73dbf70f842c3bf
MCT <zoltancao@gmail.com>
2024-10-22 22:25:19 -0300

    feature/LOC-64 corrigida a calsse de repository

--- src/main/java/com/api/domain/repository/QuantityApplicationsRepository.java
+++ src/main/java/com/api/domain/repository/QuantityApplicationsRepository.java
@@ -1,7 +1,12 @@
 package com.api.domain.repository;
 
+
+import com.api.domain.entity.FactHiring;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class QuantityApplicationsRepository {
+public interface QuantityApplicationsRepository extends JpaRepository<FactHiring, Long> {
 }
+
+
