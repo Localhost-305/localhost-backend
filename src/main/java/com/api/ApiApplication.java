43a03d073554af21c165dfb49478c5b93e554386
Lukas <lkfernando12345@gmail.com>
2024-11-07 16:13:47 -0300

    feature/LOC-72/adicionando dois novos imports

--- src/main/java/com/api/ApiApplication.java
+++ src/main/java/com/api/ApiApplication.java
@@ -2,7 +2,11 @@ package com.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(basePackages = "com.api.domain.repository")
+@EntityScan(basePackages = "com.api.domain.entity")
 @SpringBootApplication
 public class ApiApplication {
 
