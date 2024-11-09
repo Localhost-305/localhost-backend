8ba169893eb4c1fd900a82f176bc53d423ad53ce
Lukas <lkfernando12345@gmail.com>
2024-11-09 14:35:11 -0300

    feature/LOC-72/testando mudar o endpoint para o front usar o login como /api/login

--- src/main/java/com/api/controller/AuthenticationController.java
+++ src/main/java/com/api/controller/AuthenticationController.java
@@ -13,7 +13,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/login")
+@RequestMapping("/api/login")
 public class AuthenticationController {
 
     @Autowired
