42e068e8f8455395e5fa1e853ae5be3756f9456e
Lukas <lkfernando12345@gmail.com>
2024-11-09 14:53:40 -0300

    feature/LOC-72/voltando para login

--- src/main/java/com/api/controller/AuthenticationController.java
+++ src/main/java/com/api/controller/AuthenticationController.java
@@ -13,7 +13,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/login")
+@RequestMapping("/login")
 public class AuthenticationController {
 
     @Autowired
