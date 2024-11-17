202f4c82bb0e567e9adc1b0c796cb1c06f937b1b
Vitor Lima <vitor02office@gmail.com>
2024-11-17 14:25:01 -0300

    feature/LOC-71/Criado teste unitario para a controller de usuário

--- src/main/java/com/api/controller/UserController.java
+++ src/main/java/com/api/controller/UserController.java
@@ -3,6 +3,9 @@ package com.api.controller;
 import com.api.domain.dto.UserDto;
 import com.api.domain.entity.Role;
 import com.api.domain.entity.User;
+import com.api.domain.exception.UserNotFoundException;
+import com.api.domain.exception.UserRegistrationException;
+import com.api.domain.exception.UserUpdateException;
 import com.api.domain.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -20,30 +23,47 @@ public class UserController {
 
     @PreAuthorize("hasAnyAuthority('allowed_to_change')")
     @GetMapping
-    public  ResponseEntity<List<User>> getAllUsers(){
-        return ResponseEntity.ok(userService.getAllUsers());
+    public ResponseEntity<List<User>> getAllUsers() {
+        try {
+            return ResponseEntity.ok(userService.getAllUsers());
+        } catch (Exception e) {
+            throw new UserNotFoundException(e);
+        }
     }
 
     @PreAuthorize("hasAnyAuthority('allowed_to_change')")
     @PostMapping
     public ResponseEntity<Void> register(@RequestBody @Validated(UserDto.Create.class) UserDto userDto){
-        userService.save(userDto);
-        return ResponseEntity.ok().build();
+        try{
+            userService.save(userDto);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            throw new UserRegistrationException("Error while register new user", e);
+        }
     }
 
     @PreAuthorize("hasAnyAuthority('allowed_to_change')")
     @PutMapping("/{userId}")
     public ResponseEntity<Void> updateUser(@PathVariable Long userId,@RequestBody @Validated(UserDto.Update.class) UserDto userDto) {
-        userService.update(userId, userDto);
-        return ResponseEntity.ok().build();
+        try {
+            userService.update(userId, userDto);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            throw new UserUpdateException(e);
+        }
     }
 
     @PreAuthorize("hasAnyAuthority('allowed_to_change')")
     @PutMapping("/{userId}/role")
     public ResponseEntity<Void> updateRole(@PathVariable Long userId, @RequestBody String roleId) {
-        System.out.println(roleId);
-        userService.updateRole(userId, roleId);
-        return ResponseEntity.ok().build();
+
+        try{
+            System.out.println(roleId);
+            userService.updateRole(userId, roleId);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            throw new UserUpdateException(e);
+        }
     }
 
     @GetMapping("/roles")
