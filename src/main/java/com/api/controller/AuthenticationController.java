0907b3901dfe118f99861982230f52cce5da8fac
Laroy Prado <laroyprado@gmail.com>
2024-12-01 18:37:29 -0300

    feature/LOC-79/Envia email com base em eventos: 1- 3 tentativas ou mais de acessr uma conta, 2- Importação de Excel para o ETL 3- Alteração em algum dado usuário

--- src/main/java/com/api/controller/AuthenticationController.java
+++ src/main/java/com/api/controller/AuthenticationController.java
@@ -4,19 +4,24 @@ import com.api.domain.dto.DataTokenJWTDTO;
 import com.api.domain.dto.LoginDto;
 import com.api.domain.dto.UserResumeDTO;
 import com.api.domain.entity.User;
+import com.api.domain.service.EmailService;
 import com.api.domain.service.UserService;
 import com.api.infra.security.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 @RestController
@@ -25,26 +30,63 @@ public class AuthenticationController {
 
     @Autowired
     private AuthenticationManager manager;
+
     @Autowired
     private UserService userService;
+
     @Autowired
     private TokenService tokenService;
+
+    @Autowired
+    private EmailService emailService;
+
+    private static final int MAX_ATTEMPTS = 3;
+    private final ConcurrentHashMap<String, AtomicInteger> failedAttempts = new ConcurrentHashMap<>();
 
     @RequestMapping("/login")
     @PostMapping
     public ResponseEntity login(@RequestBody @Valid LoginDto loginData) {
-        var authenticationToken = new UsernamePasswordAuthenticationToken(loginData.email(), loginData.password());
-        var authentication = manager.authenticate(authenticationToken);
-        User user = (User) authentication.getPrincipal();
-        var tokenJWT = tokenService.generateToken(user);
+        String email = loginData.email();
 
-        List<String> permissions = user.getRole().getPermissions().stream()
-                .map(permission -> permission.getPermissionName())
-                .collect(Collectors.toList());
+        try {
+            var authenticationToken = new UsernamePasswordAuthenticationToken(email, loginData.password());
+            var authentication = manager.authenticate(authenticationToken);
 
+            failedAttempts.remove(email);
 
-        var userResume = new UserResumeDTO(user.getUserId(), user.getName(), user.getEmail(), user.getRole().getRoleName(), user.getRole().getId(), permissions);
+            User user = (User) authentication.getPrincipal();
+            var tokenJWT = tokenService.generateToken(user);
 
-        return ResponseEntity.ok(new DataTokenJWTDTO(tokenJWT, userResume));
+            List<String> permissions = user.getRole().getPermissions().stream()
+                    .map(permission -> permission.getPermissionName())
+                    .collect(Collectors.toList());
+
+            var userResume = new UserResumeDTO(user.getUserId(), user.getName(), user.getEmail(), user.getRole().getRoleName(), user.getRole().getId(), permissions);
+
+            return ResponseEntity.ok(new DataTokenJWTDTO(tokenJWT, userResume));
+
+        } catch (AuthenticationException ex) {
+            handleFailedLoginAttempt(email);
+            throw new BadCredentialsException("Credenciais inválidas.");
+        }
+    }
+
+    /**
+     * Lida com tentativas de login falhas.
+     *
+     * @param email O e-mail do usuário.
+     */
+    private void handleFailedLoginAttempt(String email) {
+        AtomicInteger attempts = failedAttempts.computeIfAbsent(email, k -> new AtomicInteger(0));
+        int attemptCount = attempts.incrementAndGet();
+
+        if (attemptCount >= MAX_ATTEMPTS) {
+            failedAttempts.remove(email);
+
+            String subject = "Tentativa de acesso à sua conta";
+            String message = "Detectamos 3 tentativas de login falhas na sua conta. "
+                    + "Se não foi você, por favor, entre em contato com o suporte.";
+            emailService.sendEmail(email, subject, message);
+        }
     }
 }
--- src/main/java/com/api/controller/ImportExcelToETL.java
+++ src/main/java/com/api/controller/ImportExcelToETL.java
@@ -1,35 +1,74 @@
 package com.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import java.nio.file.Paths;
+
+import com.api.domain.entity.User;
+import com.api.domain.service.EmailService;
+
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequestMapping("/excel")
 public class ImportExcelToETL {
 
-  private static final String DIRECTORY = Paths.get(System.getProperty("user.dir"), "..\\localhost-etl\\", "localhost-etl").toString();
+    private static final String DIRECTORY = Paths.get(System.getProperty("user.dir"), "..", "localhost-etl").toString();
+    private static final String DIRECTORY_ERRORS = Paths.get(System.getProperty("user.dir"), "..", "localhost-etl", "etl_project", "src", "error_files", "LOG_ERRORS_JSON").toString();
+    
+    @Autowired
+    private EmailService emailService;
 
     @PostMapping("/upload")
     public ResponseEntity<String> uploadExcel(@RequestParam("file") MultipartFile file) {
-        if (file.isEmpty()) {
-            return ResponseEntity.badRequest().body("Por favor, selecione um arquivo!");
-        }
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated()) {
+            User user = (User) authentication.getPrincipal();
 
-        try {
-            File dest = new File(DIRECTORY + file.getOriginalFilename());
-            file.transferTo(dest); 
+            if (file.isEmpty()) {
+                return ResponseEntity.badRequest().body("Por favor, selecione um arquivo!");
+            }
 
-            return ResponseEntity.ok("Arquivo salvo com sucesso em " + dest.getAbsolutePath());
-        } catch (IOException e) {
-            e.printStackTrace();
-            return ResponseEntity.status(500).body("Erro ao salvar o arquivo: " + e.getMessage());
+            try {
+                File dest = new File(DIRECTORY + File.separator + file.getOriginalFilename());
+                file.transferTo(dest);
+
+                String errorLogContent = "";
+                File errorFile = new File(DIRECTORY_ERRORS + File.separator + file.getOriginalFilename().replaceFirst("\\.[^.]+$", ".json"));
+                if (errorFile.exists()) {
+                    errorLogContent = new String(Files.readAllBytes(errorFile.toPath()), StandardCharsets.UTF_8);
+                }
+
+                String subject = "Importação de arquivo";
+                StringBuilder messageBuilder = new StringBuilder();
+                messageBuilder.append("Olá ").append(user.getName()).append(",\n\nSeu arquivo foi importado com sucesso, os dados foram processados e adicionados.");
+
+                if (!errorLogContent.isEmpty()) {
+                    messageBuilder.append("\n\nEntretanto, ocorreram alguns erros durante o processamento:\n").append(errorLogContent);
+                }
+
+                String message = messageBuilder.toString();
+
+                emailService.sendEmail(user.getEmail(), subject, message);
+
+                return ResponseEntity.ok("Arquivo salvo com sucesso em " + dest.getAbsolutePath());
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                return ResponseEntity.status(500).body("Erro ao salvar o arquivo: " + e.getMessage());
+            }
+        } else {
+            return ResponseEntity.status(403).body("Usuário não autenticado!");
         }
     }
 }
